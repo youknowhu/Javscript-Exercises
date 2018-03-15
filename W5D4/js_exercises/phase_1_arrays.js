5dcefe92c070fd8b9c84e2ff3dcc9aa438c3cbd2
Kimmy Allgeier <kimberly.hu@gmail.com>
2018-03-15 13:43:44 -0700

    Finish JS exercises phase 1

---
+++ W5D4/js_exercises/phase_1_arrays.js
@@ -0,0 +1,38 @@
+Array.prototype.uniq = function() {
+  const arr = [];
+  this.forEach((el) => {
+    if (!arr.includes(el)) {
+      arr.push(el);
+    }
+  });
+  return arr;
+};
+
+Array.prototype.twoSum = function() {
+  const result = [];
+
+  for(let i = 0; i < this.length; i++ ) {
+    for(let j = i + 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        result.push([i, j]);
+      }
+    }
+  }
+  return result;
+};
+
+Array.prototype.transpose = function() {
+  const result = (new Array(this.length)).fill(0).map(
+    function() {
+      return new Array(this.length);
+    }
+  );
+
+  for(let i = 0; i < this.length; i++ ) {
+    for(let j = 0; j < this.length; j++) {
+      result[j][i] = this[i][j];
+    }
+  }
+
+  return result;
+};
